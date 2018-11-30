3b5625f66186bb41f1c299c4cb34c49578068a8b
Twinkle Singh <twinklechauhan2000@gmail.com>
2018-11-30 02:20:55 -0600

    Third Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import * as d3 from "d3";
+import { AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'vensiwf';
+  
+  radius = 10;
+
+  ngAfterContentInit() {
+    d3.select('p').style('color', 'red');
+  }
+
+  colorMe() {
+    d3.select('button').style('color', 'red');
+  }
+
+  clicked(event: any) {
+    d3.select(event.target).append('circle')
+      .attr('cx', event.x)
+      .attr('cy', event.y)
+      .attr('r', () => {
+        return this.radius;
+      })
+      .attr('fill', 'red');
+  }
+
+
+
 }
